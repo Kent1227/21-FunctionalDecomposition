48b17b0f615a0b61d6df98fc74536a028e4d4880
smithkg <smithkg@rose-hulman.edu>
2019-01-24 15:13:28 -0500

    done

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -4,7 +4,7 @@ Hangman.
 Authors: Kent Smith and Eddie Mannan.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
-# TODO: 2. Implement Hangman using your Iterative Enhancement Plan.
+# DONE: 2. Implement Hangman using your Iterative Enhancement Plan.
 
 # Do NOT attempt this assignment before class! #######
 
